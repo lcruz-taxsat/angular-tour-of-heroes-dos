03b00b996a179670cd42607cc4d5f98896b04fb0
lcruz-taxsat <lizbeth.cruz@taxsat.com>
2020-01-17 17:26:00 -0600

    displayin a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heros-dos';
+  title = 'Tour of heroes';
 }
