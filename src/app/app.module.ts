5fca73d7726f03cb964dbf3d940295caa4c676df
lcruz-taxsat <lizbeth.cruz@taxsat.com>
2020-01-18 14:45:23 -0600

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessageComponent } from './messages/message.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessageComponent } from './messages/message.component'; // <-- NgModel lives here
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessageComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/messages/message.component.ts
+++ src/app/messages/message.component.ts
@@ -3,7 +3,7 @@ import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-message',
-  templateUrl: './messages.component.html',
+  templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss']
 })
 export class MessageComponent implements OnInit {
